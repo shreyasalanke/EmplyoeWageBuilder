57bfafefdcb02f162fca1f924fb9b71785d6c3dd
shreyasalanke <shreyasalanke97@gmail.com>
2021-06-22 13:54:57 +0530

    ADDED: employewagebuilder to check whether emplyoe is present or absent

---
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+import java.util.Random;
+
+public class EmpWageBuilder {
+	
+	
+	public static void main(String[] args) {
+		
+		double empCheck =Math.floor(Math.random()*10)%2;
+		
+		if (empCheck == 1 ){
+			System.out.println("emp is prsent");
+		}
+		else
+		{
+			System.out.println("emp absent");
+				
+		}
+		
+			
+	}
+	
+	
+
+}

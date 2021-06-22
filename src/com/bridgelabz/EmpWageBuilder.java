e43c539cc4b311eb7a1cf5c343af3224c278794c
shreyasalanke <shreyasalanke97@gmail.com>
2021-06-22 16:15:37 +0530

    MODIFIED: check emp wage to calculate daily emp wage

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -7,20 +7,27 @@ public class EmpWageBuilder {
 	
 	public static void main(String[] args) {
 		
+		int IS_EMP_PRESENT = 1;
+		int EMP_WAGE_PER_HOUR = 20;
+		int FULL_DAY_HOUR = 8;
+		
 		double empCheck =Math.floor(Math.random()*10)%2;
 		
-		if (empCheck == 1 ){
+		if (empCheck == IS_EMP_PRESENT )
+		{
 			System.out.println("emp is prsent");
+			int dailyEmpWage = EMP_WAGE_PER_HOUR * EMP_WAGE_PER_HOUR;
+			System.out.println("emp wage is:"+dailyEmpWage);
 		}
 		else
 		{
 			System.out.println("emp absent");
+			int dailyEmpWage = 0;
+			
 				
 		}
 		
 			
-	}
-	
-	
+	}	
 
 }
